a587d0a60c3ea0cf198e3d610b872238ab08a731
Guilherme Alves <guilherme.alves.lourenco@gmail.com>
2026-07-27 15:31:02 -0300

    fix: tailwind prettier plugin

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-const Dashboard = () => {
-  return ( <h1>Dashboard Page</h1> );
-}
- 
-export default Dashboard;
+const Home = () => {
+  return (
+    <div className="flex w-screen items-center justify-center">
+      <h1 className="p-5 text-red-500">Dashboard Page</h1>
+    </div>
+  );
+};
+
+export default Home;
