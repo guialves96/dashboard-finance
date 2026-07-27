6c63ced710a43e348fd96d58e655eddcbb4041ae
Guilherme Alves <guilherme.alves.lourenco@gmail.com>
2026-07-27 14:59:20 -0300

    chore: initialize Dashboard Finance project

---
+++ app/subscription/page.tsx
@@ -0,0 +1,5 @@
+const subscription = () => {
+    return ( <h1>Subscrition Page</h1> );
+}
+ 
+export default subscription;
---
+++ app/transactions/page.tsx
@@ -0,0 +1,5 @@
+const transactions = () => {
+    return ( <h1>Transactions Page</h1> );
+}
+ 
+export default transactions;
