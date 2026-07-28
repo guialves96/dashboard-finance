e0f1a5e239d126748572010c8a20667bcfcb6327
Guilherme Alves <guilherme.alves.lourenco@gmail.com>
2026-07-28 15:49:30 -0300

    feat: implement Clerk authentication

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,23 +1,40 @@
 import Image from "next/image";
 import { Button } from "../_components/ui/button";
 import { LogInIcon } from "lucide-react";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const LoginPage = () => {
+const LoginPage = async () => {
+  const { userId } = await auth();
+
+  if (userId) {
+    redirect("/");
+  }
+
   return (
     <div className="grid h-full grid-cols-2">
       {/* ESQUERDA */}
-      <div className="mx-auto mb-8 flex h-full max-w-[550px] flex-col justify-center p-8">
-        <Image src="/logo.svg" alt="Finance AI" width={173} height={39} />
+      <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
+        <Image
+          src="/logo.svg"
+          alt="Finance AI"
+          width={173}
+          height={39}
+          className="mb-8"
+        />
         <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
         <p className="mb-8 text-muted-foreground">
           A Finance AI é uma plataforma de gestão financeira que utiliza IA para
           monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle do seu orçamento.
         </p>
-        <Button variant="outline">
-          <LogInIcon className="mr-2" />
-          Fazer login ou criar conta
-        </Button>
+        <SignInButton>
+          <Button variant="outline">
+            <LogInIcon className="mr-2" />
+            Fazer login ou criar conta
+          </Button>
+        </SignInButton>
       </div>
 
       {/* DIREITA */}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-import { Button } from "./_components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const Home = () => {
+const Home = async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/login");
+  }
+
   return (
-    <div className="flex w-screen items-center justify-center">
-      <Button> Hello World </Button>
+    <div className="mt-10 flex w-screen items-center justify-center text-white">
+      <UserButton showName />
     </div>
   );
 };
