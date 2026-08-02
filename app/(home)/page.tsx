7168f8206082913c9446b35976bb0a4acf008afe
Guilherme Alves <guilherme.alves.lourenco@gmail.com>
2026-08-02 17:12:11 -0300

    feat: add transaction pie chart component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,15 +1,15 @@
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import Navbar from "../_components/navbar";
 import SummaryCards from "./_components/summary-cards";
 import TimeSelect from "./_components/time-select";
 import { isMatch } from "date-fns";
-// import TransactionsPieChart from "./_components/transactions-pie-chart";
-// import { getDashboard } from "../_data/get-dashboard";
-// import ExpensesPerCategory from "./_components/expenses-per-category";
-// import LastTransactions from "./_components/last-transactions";
-// import { canUserAddTransaction } from "../_data/can-user-add-transaction";
-// import AiReportButton from "./_components/ai-report-button";
+import TransactionsPieChart from "./_components/transactions-pie-chart";
+import { getDashboard } from "../_data/get-dashboard";
+import ExpensesPerCategory from "./_components/expenses-per-category";
+import LastTransactions from "./_components/last-transactions";
+import { canUserAddTransaction } from "../_data/can-user-add-transaction";
+import AiReportButton from "./_components/ai-report-button";
 
 interface HomeProps {
   searchParams: {
@@ -26,9 +26,9 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
   if (monthIsInvalid) {
     redirect(`?month=${new Date().getMonth() + 1}`);
   }
-  // const dashboard = await getDashboard(month);
-  // const userCanAddTransaction = await canUserAddTransaction();
-  // const user = await clerkClient().users.getUser(userId);
+  const dashboard = await getDashboard(month);
+  const userCanAddTransaction = await canUserAddTransaction();
+  const user = await clerkClient().users.getUser(userId);
   return (
     <>
       <Navbar />
@@ -36,12 +36,12 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
         <div className="flex justify-between">
           <h1 className="text-2xl font-bold">Dashboard</h1>
           <div className="flex items-center gap-3">
-            {/* <AiReportButton
+            <AiReportButton
               month={month}
               hasPremiumPlan={
                 user.publicMetadata.subscriptionPlan === "premium"
               }
-            /> */}
+            />
             <TimeSelect />
           </div>
         </div>
@@ -49,17 +49,17 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
           <div className="flex flex-col gap-6 overflow-hidden">
             <SummaryCards
               month={month}
-              // {...dashboard}
-              // userCanAddTransaction={userCanAddTransaction}
+              {...dashboard}
+              userCanAddTransaction={userCanAddTransaction}
             />
             <div className="grid h-full grid-cols-3 grid-rows-1 gap-6 overflow-hidden">
-              {/* <TransactionsPieChart {...dashboard} /> */}
-              {/* <ExpensesPerCategory */}
-              {/* expensesPerCategory={dashboard.totalExpensePerCategory}
-              /> */}
+              <TransactionsPieChart {...dashboard} />
+              <ExpensesPerCategory
+                expensesPerCategory={dashboard.totalExpensePerCategory}
+              />
             </div>
           </div>
-          {/* <LastTransactions lastTransactions={dashboard.lastTransactions} /> */}
+          <LastTransactions lastTransactions={dashboard.lastTransactions} />
         </div>
       </div>
     </>
