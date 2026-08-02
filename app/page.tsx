edee1b18cce77d9c53624e90ba75c3b2cdc7efc1
Guilherme Alves <guilherme.alves.lourenco@gmail.com>
2026-08-02 15:22:38 -0300

    feat: add navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { UserButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import Navbar from "./_components/navbar";
 
 const Home = async () => {
   const { userId } = await auth();
@@ -10,9 +11,12 @@ const Home = async () => {
   }
 
   return (
-    <div className="mt-10 flex w-screen items-center justify-center text-white">
-      <UserButton showName />
-    </div>
+    <>
+      <Navbar />
+      <div className="mt-10 flex w-screen items-center justify-center text-white">
+        <UserButton showName />
+      </div>
+    </>
   );
 };
 
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -2,18 +2,22 @@ import { db } from "../_lib/prisma";
 import { DataTable } from "../_components/ui/data-table";
 import { transactionsColumns } from "./_columns";
 import AddTransactionButton from "../_components/add-transaction-button";
+import Navbar from "../_components/navbar";
 
 const TransactionsPage = async () => {
   // acessar as transações do banco de dados
   const transactions = await db.transaction.findMany({});
   return (
-    <div className="p-6">
-      <div className="flex w-full items-center justify-between p-6">
-        <h2 className="text-2xl font-bold">Transações</h2>
-        <AddTransactionButton />
+    <>
+      <Navbar />
+      <div className="p-6">
+        <div className="flex w-full items-center justify-between p-6">
+          <h2 className="text-2xl font-bold">Transações</h2>
+          <AddTransactionButton />
+        </div>
+        <DataTable columns={transactionsColumns} data={transactions} />
       </div>
-      <DataTable columns={transactionsColumns} data={transactions} />
-    </div>
+    </>
   );
 };
 
