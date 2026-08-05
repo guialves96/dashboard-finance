dacc8e7b5d1d4a9ad80e015a019501856f3e7901
Guilherme Alves <guilherme.alves.lourenco@gmail.com>
2026-08-05 12:44:16 -0300

    feat: ensure user can't create transaction on free plan if above limit

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -6,32 +6,36 @@ import Navbar from "../_components/navbar";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { ScrollArea } from "../_components/ui/scroll-area";
+import { canUserAddTransaction } from "../_data/can-user-add-transaction";
 
 const TransactionsPage = async () => {
-  // Validação de login
   const { userId } = await auth();
-
   if (!userId) {
     redirect("/login");
   }
-
-  // acessar as transações do banco de dados
   const transactions = await db.transaction.findMany({
     where: {
       userId,
     },
+    orderBy: {
+      date: "desc",
+    },
   });
-
+  const userCanAddTransaction = await canUserAddTransaction();
   return (
     <>
       <Navbar />
-      <div className="space-y-6 overflow-hidden p-6">
-        <div className="flex w-full items-center justify-between p-6">
-          <h2 className="text-2xl font-bold">Transações</h2>
-          <AddTransactionButton />
+      <div className="flex flex-col space-y-6 overflow-hidden p-6">
+        {/* TÍTULO E BOTÃO */}
+        <div className="flex w-full items-center justify-between">
+          <h1 className="text-2xl font-bold">Transações</h1>
+          <AddTransactionButton userCanAddTransaction={userCanAddTransaction} />
         </div>
-        <ScrollArea>
-          <DataTable columns={transactionsColumns} data={transactions} />
+        <ScrollArea className="h-full">
+          <DataTable
+            columns={transactionsColumns}
+            data={JSON.parse(JSON.stringify(transactions))}
+          />
         </ScrollArea>
       </div>
     </>
